a40d1f7424f14f831144fcfcb34ec743a796db76
biancabritto-wq <bianca.britto@edu.senai.br>
2026-07-29 15:52:35 -0300

    Aula 1 back-end

---
+++ condicao_if.php
@@ -0,0 +1,21 @@
+<?php
+$tempo = "nublado";
+
+if ($tempo == "chuva") {
+    echo ("está $tempo e não vou à escola ");
+} 
+elseif ($tempo == "sol") {
+    echo ("está $tempo e vou a escola ");
+}
+elseif ($tempo == "nublado") {
+    echo ("está $tempo e vou a escola ");
+}
+elseif ($tempo == "nevando") {
+    echo ("está $tempo e vou a escola ");
+}
+else {
+    echo "Esta acontecendo tudo! Menos chuva então vou para a escola";
+}
+
+
+
---
+++ resto_divisao.php
@@ -0,0 +1,9 @@
+<?php
+
+$num1 = 10;
+$num2 = 3;
+$divisao = ($num1 / $num2);
+$resto =  $num1 % $num2 ;
+echo("O resto de $num1 dividido por $num2 é: ");
+echo $resto; //Saída: 1 (pois 10 dividido por 3 tem resto 1 )
+?>
